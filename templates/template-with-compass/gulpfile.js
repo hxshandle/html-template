8be3b3f9130ba82ca5b86fc052a20bb364cd60d6
hxshandle <handle.huang0409@gmail.com>
2015-01-24 19:33:01 +0800

    add package json

--- templates/template-with-compass/gulpfile.js
+++ templates/template-with-compass/gulpfile.js
@@ -1,5 +1,8 @@
 var gulp = require('gulp');
 var jade = require('gulp-jade');
+var sass = require('gulp-ruby-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var path = require('path');
 
 gulp.task('compile-jade', function() {
   console.log('compile-jade');
@@ -10,7 +13,34 @@ gulp.task('compile-jade', function() {
     .pipe(gulp.dest('./'));
 });
 
+gulp.task('compile-scss', function() {
+  console.log('compile-scss');
+  gulp.src('./scss/**/[!^_]*.scss')
+    .pipe(sass())
+    //.pipe(sourcemaps.write('./maps'))
+    .pipe(gulp.dest('css'));
+});
+
+gulp.task('styles', function() {
+  return gulp.src('scss/**/[!^_]*.scss')
+    .on('data', function(file) {
+      if (process.platform == "win32") {
+        file.path = path.relative(".", file.path);
+        file.path = file.path.replace(/\\/g, "/");
+      }
+    }).pipe(sass({
+      sourceComments: 'map'
+    }))
+    .pipe(gulp.dest('.tmp/styles'));
+});
+
+
 gulp.task('watch-jade', function() {
   gulp.watch('jade/**/*.jade', ['compile-jade']);
 });
-gulp.task('default', ['watch-jade']);
+
+gulp.task('watch-scss', function() {
+  gulp.watch('scss/**/*.scss', ['compile-scss']);
+});
+
+gulp.task('default', ['watch-jade','watch-scss']);
